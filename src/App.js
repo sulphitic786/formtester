0d960dbad12356df1f8ba3f0ad8122c7f7d33d49
sulphitic786 <wqasimg@gmail.com>
2022-01-13 09:00:24 +0500

    13/22 morning

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//--------------- Tester Form -------------
+import React from "react";
+import TForm from './tester/TForm';
+import StyleTester from './tester/StyleTester.css';
+
+
+const App = () => {
+  return (
+    <>
+      <TForm />
+    </>
+  )
+}
+
+export default App;
